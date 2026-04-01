2a87ab5f12270c1c8f1b3adb30c62179559db23d
TH_NexusOne <thalysons122@gmail.com>
2026-04-01 01:07:07 -0300

    Add files via upload

--- js/ui/transactions-ui.js
+++ js/ui/transactions-ui.js
@@ -1432,6 +1432,9 @@ export function bindTxEvents() {
   if (window.flatpickr) {
     const commonDateCfg = { 
       dateFormat: "d/m/Y", locale: "pt", allowInput: true, disableMobile: true, static: false, position: "auto",
+      onOpen: function(selectedDates, dateStr, instance) {
+        instance.calendarContainer.classList.add('glass');
+      },
       nextArrow: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>',
       prevArrow: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>',
       onReady: function(selectedDates, dateStr, instance) {
